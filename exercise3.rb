26fc07f788a5b4c12f9c94464a0e4be905a776a2
Taha Jalil <taha.jalil@gmail.com>
2014-03-25 14:22:00 -0400

    completed exercise3.rb

--- exercise3.rb
+++ exercise3.rb
@@ -1,13 +1,15 @@
-=begin
 puts "What is your name?"
-name = gets.chomp
+name = gets.chomp.capitalize!
 
 puts "Hi #{name}"
 
 puts "Also, how old are you?"
 age = gets.chomp.to_i #added .to_i as every value inputted by user is string by default. We want integer.
-=end
+
 
 require 'date'
 current_year = Date.today.strftime("%Y").to_i
-puts current_year
+
+birth_year = current_year - age
+
+puts "You are #{age}. This means you must have been born in #{birth_year}. Bummer..."
