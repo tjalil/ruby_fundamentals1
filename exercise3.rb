fd832b3156fac96280be6201f5d0fd3895ef9e4c
Taha Jalil <taha.jalil@gmail.com>
2014-03-25 14:17:52 -0400

    required 'Date', created current_year, convert current_year to_i and puts to check if it works

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,13 @@
+=begin
 puts "What is your name?"
 name = gets.chomp
+
 puts "Hi #{name}"
+
+puts "Also, how old are you?"
+age = gets.chomp.to_i #added .to_i as every value inputted by user is string by default. We want integer.
+=end
+
+require 'date'
+current_year = Date.today.strftime("%Y").to_i
+puts current_year
